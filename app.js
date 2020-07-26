7de9a90d1bcc46d7021669d9f6fb84515e91c994
nina1012 <labferment10@gmail.com>
2020-07-26 15:23:12 +0200

    Initial commit. Add basic markup and style

---
+++ app.js
@@ -0,0 +1,35 @@
+function add(a, b) {
+  return a + b;
+}
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+function operate(operator, num1, num2) {
+  switch (operator) {
+    case '+':
+      return add(num1, num2);
+      break;
+    case '-':
+      return subtract(num1, num2);
+      break;
+    case '*':
+      return multiply(num1, num2);
+      break;
+    case '/':
+      return divide(num1, num2);
+  }
+}
+
+console.log(operate('+', 10, 17));
+console.log(operate('-', 20, 17));
+console.log(operate('*', 5, 12));
+console.log(operate('/', 50, 5));
